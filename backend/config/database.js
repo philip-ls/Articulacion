8e2900792efe9f23b2367ff1e7d9f7242b88f4c0
philip-ls <felipo89lopez@hotmail.com>
2026-02-09 16:59:23 -0500

    Create database.js

---
+++ backend/config/database.js
@@ -0,0 +1,30 @@
+/**CONFIGURACION DE LA BASE DE DATOS */
+
+// Importar sequelize
+const { Sequelize } = require("sequelize");
+
+//Importar dotenv para variables| de entorno
+require("dotenv").config();
+
+//Crear una instancia de Sequelize
+const sequelize = new Sequelize(
+  Process.env.DB_NAME,
+  Process.env.DB_USER,
+  Process.env.DB_PASSWORD,
+  {
+    host: Process.env.DB_HOST,
+    port: Process.env.DB_PORT,
+    dialect: "mysql",
+
+    //configuración de pool de conexiones
+    //mantiene las conexiones abiertas para mejorar el rendimiento
+    pool: {
+      max: 5, //Número máximo de conexiones en el pool
+      min: 0, //Número mínimo de conexiones en el pool
+      acquire: 30000, //Tiempo máximo (en ms) que sequelize intentará obtener una conexión antes de lanzar un error
+      idle: 10000, //Tiempo máximo (en ms) que una conexión puede estar inactiva antes de ser liberada
+    },
+  },
+);
+
+module.exports = sequelize;
