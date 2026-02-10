d1db61c9324106ba6cc01602562187c35d707bb4
philip-ls <felipo89lopez@hotmail.com>
2026-02-10 16:49:41 -0500

    Update jwt.js

--- backend/config/jwt.js
+++ backend/config/jwt.js
@@ -40,7 +40,53 @@ const generateToken = (payload) => {
 /**
  * Verificar un token JWT
  * 
- * @param {string} token - Token JWT a verificar
+ * @param {string} tokenHeader - Token JWT a verificar
  * @returns {object} - Datis decodificados del token si es valido
  * @throws {Error} - Si el token es valido o ha expirado
  */
+
+const verifyToken = (tokenHeader) => {
+  try {
+    //jwt.verify() verifica la firma del token y lo decodifica
+    //Parametros:
+    //1. token: el token JTW a verificat
+    //2. secret: la misma clave secreta usada para firmarlo
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    return decoded;
+  } catch (error) {
+    //Diferentes tipos de errores 
+    if (error.name === "TokenExpiredError") {
+      throw new Error("Token Expirado");
+    } else if (error.name === "JsonWebTokenerror"){
+      throw new Error("Token Invalido");
+    } else {
+      throw new error("Error al verificar token");
+    }
+  }
+};
+
+/**
+ * Extraer el token del header autorization
+ * El token viene en formato "Bearer <token>"
+ * 
+ * @param {string} authHeader - Header Autorization de la petición
+ * @returns {string|null} - Token extraido o null si no existe
+ */
+
+const extractToken  = (authHeader) => {
+  // verifica que el header existe y empieza con "Bearer "
+  if (authHeader && authHeader.startsWith("Bearer")) {
+   //Extraer solo el token (Quitar "Bearer")
+  return authHeader.substring(7);
+  } 
+
+  return null; // Si no hay token o formato incorrecto
+};
+
+// Exportar las funciones para usarlas en otros archivos
+module.exports = {
+  generateToken,
+  verifyToken,
+  extractToken,
+};
